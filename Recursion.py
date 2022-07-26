8465e5e80e603d48855d227cafe47eeee88e091c
chewzzz <chewziqing@gmail.com>
2022-07-26 14:35:22 +0800

    Initial Commit

---
+++ Recursion.py
@@ -0,0 +1,16 @@
+# recursive function that uses the call stack
+
+# call stack (bottom to top) for x = 3:
+# {} => {x=3} => {x=3, x=2} => {x=3, x=2, x=1}
+
+# when x = 1, return 1. The stack x=1 is removed:
+# { x=3, x=2, x=1} => return 1 => {x=3, x=2} => return (2*factorial(1)) => {x=3} => return (3*factorial(2)) => {}
+
+def factorial(x):
+    # base case
+    if x == 1:
+        return 1
+
+    # recursive case
+    else:
+        return x * factorial(x-1)
