c5d233fdc4d8ef0776d0cad6e4e5d0c2972a961f
chewzzz <chewziqing@gmail.com>
2022-07-26 14:40:19 +0800

    Added more comment

--- Recursion.py
+++ Recursion.py
@@ -4,7 +4,7 @@
 # {} => {x=3} => {x=3, x=2} => {x=3, x=2, x=1}
 
 # when x = 1, return 1. The stack x=1 is removed:
-# { x=3, x=2, x=1} => return 1 => {x=3, x=2} => return (2*factorial(1)) => {x=3} => return (3*factorial(2)) => {}
+# { x=3, x=2, x=1} => return 1 => {x=3, x=2} => return (2*factorial(1)), that is 2*1, 2 => {x=3} => return (3*factorial(2)), that is 3*2, 6 => {}
 
 def factorial(x):
     # base case
