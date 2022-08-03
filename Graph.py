657b785f0ece1f5e62cd96aab0ce8cd1441fbfd2
chewzzz <chewziqing@gmail.com>
2022-08-03 15:16:17 +0800

    Added checked list of ppl

--- Graph.py
+++ Graph.py
@@ -15,31 +15,41 @@ graph["peggy"] = []
 graph["thom"] = []
 graph["johnny"] = []
 
-# create a new queue
-search_queue = deque()
-
-# add all of ur neighbors to queue
-search_queue += graph["you"]
-
 # using breadth-first search to find ur connection
 def person_is_connection(name):
     # let's assume that all my connection has name ends with letter "c"
     return (name[-1] == "c")
 
-def bfs(search_queue):
-    while search_queue:
-        # while queue is not empty
+def search_connection(name):
+    # create a new queue
+    search_queue = deque()
 
+    # add all of ur neighbors to queue
+    search_queue += graph["you"]
+
+    # before checking a person, make sure they haven't been checked already
+    # keep a list of people already checked
+    searched = []
+
+    # while queue is not empty
+    while search_queue:
         # pop the first person from the queue
         person = search_queue.popleft()
 
-        # is the person is my connection?
-        if person_is_connection(person):
-            # yes it's
-            print(person+"is my connection!")
-            return True
-        else:
-            # nope. add all of this person's connection to search queue
-            search_queue += graph[person]
+        # only search this person if haven't search them
+        if not (person in searched):
+            # is the person is my connection?
+            if person_is_connection(person):
+                # yes it's
+                print(person+"is my connection!")
+                return True
+            else:
+                # nope. add all of this person's connection to search queue
+                search_queue += graph[person]
+                # marked this person as searched
+                searched.append(person)
 
     return False
+
+
+print("Is my connection found?", search_connection("you") )
