8ae18643ea2ebb26bdc20c5e2e422b335c594349
chewzzz <chewziqing@gmail.com>
2022-11-02 22:04:08 +0800

    Add unordered linked list

---
+++ python_implementation/linked_list/unordered_linked_list.py
@@ -0,0 +1,73 @@
+# implementation of linked list in python
+# each item (also known as node) in the list hold data and reference to next node
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+    def getData(self):
+        return self.data
+
+    def getNext(self):
+        return self.next
+
+    def setData(self, data):
+        self.data = data
+
+    def setNext(self, next):
+        self.next = next
+
+
+# linked list must maintain a reference to the first node
+# list itself does not contain any node object, but it only contains a single reference to first node
+class UnorderedList:
+    def __init__(self):
+        self.head = None
+
+    def isEmpty(self):
+        return self.head == None
+
+    # newly inserted item becomes head of list
+    def add(self, item):
+        temp = Node(item)
+        temp.setNext(self.head)
+        self.head = temp
+
+    # method size, search, remove are based on linked list traversal
+    def size(self):
+        current = self.head
+        count = 0
+        # traversal
+        while current != None:
+            count += 1
+            current = current.getNext()
+        return count
+
+    def search(self, item):
+        current = self.head
+        found = False
+        while current!=None and not found:
+            if current.getData() == item:
+                found = True
+            else:
+                current = current.getNext()
+        return found
+
+    def remove(self, item):
+        current = self.head
+        previous = None
+        found = False
+
+        while not found:
+            if current.getData() == item:
+                found = True
+            else:
+                previous = current
+                #  known as “inch-worming” as previous must catch up to current before current moves ahead
+                current = current.getNext()
+
+        if previous == None:
+            self.head = current.getNext()
+        else:
+            previous.setNext(current.getNext())
