9964073c66af992bc6f4e15b810ce46de1de57af
chewzzz <chewziqing@gmail.com>
2022-12-05 19:30:09 +0800

    Add Process class and main class

---
+++ SchedulingAlgorithm/Process.java
@@ -0,0 +1,9 @@
+
+public class Process {
+	int arrivalTime, burstTime, waitTime, turnAroundTime;
+	
+	public Process(int arrivalTime, int burstTime) {
+		this.arrivalTime = arrivalTime;
+		this.burstTime = burstTime;
+	}
+}
