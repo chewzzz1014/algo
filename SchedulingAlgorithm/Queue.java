e1a500ee82c23f6fba2d2dbdc4382dc2a7ddb9f9
chewzzz <chewziqing@gmail.com>
2022-12-05 19:23:15 +0800

    Delete test file and add Queue.java

---
+++ SchedulingAlgorithm/Queue.java
@@ -0,0 +1,47 @@
+
+public class Queue {
+	private Object[] objQ;
+	private int frontIdx, backIdx, size;
+	
+	public Queue (int capacity) {
+		objQ = new Object[capacity];
+		frontIdx = 0;
+		backIdx = capacity - 1;
+		size = 0;
+	}
+	
+	public void enqueue(Object obj) {
+		ensureCapacity();
+		backIdx = (backIdx+1) % objQ.length;
+		objQ[backIdx] = obj;
+		size ++;
+	}
+	
+	public Object dequeue() {
+		Object front;
+		
+		if (size == 0)
+			throw new IllegalStateException("queue is empty");
+		
+		front = objQ[frontIdx];
+		frontIdx = (frontIdx + 1) % objQ.length;
+		size --;
+		
+		return front;
+	}
+	
+	private void ensureCapacity() {
+		if(size == objQ.length) {
+			Object[] oldQ = objQ;
+			
+			objQ = new Object[2 * oldQ.length];
+			
+			for (int i=0; i < oldQ.length; i++) {
+				objQ[i] = oldQ[frontIdx];
+				frontIdx = (frontIdx + 1) % oldQ.length;
+			}
+			frontIdx = 0;
+			backIdx = oldQ.length - 1;
+		}
+	}
+}
