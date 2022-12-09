38c8381625966a9e4bf142a8fa2c050be7a90b2d
chewzzz <chewziqing@gmail.com>
2022-12-09 16:30:51 +0800

    Add comments in FCFSScheduling.java

--- SchedulingAlgorithm/FCFSScheduling.java
+++ SchedulingAlgorithm/FCFSScheduling.java
@@ -1,13 +1,18 @@
+// Chew Zi Qing 212360
+
+// implementing First Come First Serve scheduling algorithm
 public class FCFSScheduling {
 	public static void main (String[]args) {
 		
-		// Queue
+		// processes' FIFO queue
 		Queue FCFCQueue = new Queue(5);
+		// initialize statistics
 		int currentTime = 0,
 			totalTurnAroundTime = 0,
 			totalWaitTime = 0,
 			numProcess = 0;
 		
+		// add 5 processes to the queue
 		FCFCQueue.enqueue(new Process("A", 0, 6));
 		FCFCQueue.enqueue(new Process("B", 2, 4));
 		FCFCQueue.enqueue(new Process("C", 5, 1));
@@ -15,6 +20,8 @@ public class FCFSScheduling {
 		FCFCQueue.enqueue(new Process("E", 	9, 5));
 		numProcess = FCFCQueue.size();
 		
+		// calculate complete time, turn around time and waiting time for each process
+		// print statistics for each process
 		System.out.println("Process\tArrival\tBurst\tComplete\tTurnaround\tWaiting");
 		for (int i=0; i<numProcess; i++) {
 			Process currentProcess = (Process) FCFCQueue.dequeue();
@@ -25,9 +32,10 @@ public class FCFSScheduling {
 			
 			totalTurnAroundTime += currentProcess.turnAroundTime;
 			totalWaitTime += currentProcess.waitTime;
-			System.out.print(currentProcess.toString());;
+			System.out.print(currentProcess.toString());
 		}
 		
+		// output the average turn around time and waiting time
 		System.out.println();
 		System.out.printf("Average Turn Around Time: %.2f units\n", ((double)totalTurnAroundTime/numProcess));
 		System.out.printf("Average Waiting Time: %.2f units\n", ((double)totalWaitTime/numProcess));
