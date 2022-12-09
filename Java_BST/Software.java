e529a74127d7bce282fce557d37dd802dcef89cb
chewzzz <chewziqing@gmail.com>
2022-12-09 23:09:42 +0800

    Add Software.java

---
+++ Java_BST/Software.java
@@ -0,0 +1,16 @@
+
+public class Software {
+	private String name, version;
+	private int quantity, price, position;
+	
+	public Software(String name, String version, int quantity, int price) {
+		this.name = name;
+		this.version = version;
+		this.quantity = quantity;
+		this.price = price;
+	}
+	
+	public String getKey() {
+		return name+" "+version;
+	}
+}
