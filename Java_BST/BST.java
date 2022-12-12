79ecf6530573a21d6a005852405b5be9ccb22bc5
chewzzz <chewziqing@gmail.com>
2022-12-12 22:54:52 +0800

    Add BST methods

---
+++ Java_BST/BST.java
@@ -0,0 +1,51 @@
+
+public class BST<T> {
+	
+	T root;
+	BST left, right;
+	
+	public BST(T root) {
+		this.root = root;
+	}
+	
+	public boolean search(BST t, int n) {
+		return true;
+	}
+	
+	public void addNode(BST t, int newItem) {
+		
+	}
+	
+	public boolean isBSTEmpty(BST t) {
+		return true;
+	}
+	
+	public void insertNode(BST t, int newItem) {
+		
+	}
+	
+	public void findDescendant(BST t, BST q) {
+		
+	}
+	
+	public void deleteNode(BST t) {
+		
+	}
+	
+	public void destroy(BST t) {
+		
+	}
+	
+	public void preOrderBST(BST t) {
+		
+	}
+	
+	public void inOrderBST(BST t) {
+		
+	}
+	
+	public void postOrderBST(BST t) {
+		
+	}
+	
+}
