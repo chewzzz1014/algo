54b62bdcf3c3959a08d31fdbbae9b0ae04356979
chewzzz <chewziqing@gmail.com>
2022-12-16 22:31:50 +0800

    Add comment to BinarySearchTree.java

--- Java_BST/BinarySearchTree.java
+++ Java_BST/BinarySearchTree.java
@@ -1,10 +1,17 @@
+// Chew Zi Qing 212360
+
+// BST implementation
 class BinarySearchTree<T>{
+  final static String FILEPATH = "C:\\Users\\USER\\eclipse-workspace\\Algo_DS\\Java_BST\\Software.txt";
+  final static String FILEPATH_TEMP = "C:\\Users\\USER\\eclipse-workspace\\Algo_DS\\Java_BST\\Software_tmp.txt";
   BSTNode<T> root;
 
+  // start with empty BST
   BinarySearchTree() {
     root = null;
   }
 
+  // insert a node in alphabetically ascending order
   void insert(T key, int pos) {
     root = insertKey(root, key, pos);
   }
@@ -18,25 +25,25 @@ class BinarySearchTree<T>{
 
     String targetNode = ((Software)(target)).key;
     String rootNode = ((Software)(root.data)).key;
-    // Traverse to the right place and insert the node
+    // add as left node if the key is alphabetically smaller
     if (targetNode.compareTo(rootNode)<0)
       root.left = insertKey(root.left, target, pos);
+   // add as right node if the key is alphabetically larger
     else if (targetNode.compareTo(rootNode)>0)
       root.right = insertKey(root.right, target, pos);
     else {
-    	// software already existed. update quantity and price
+    	// software already existed. Update quantity and price
     	((Software)(root.data)).quantity = ((Software)(target)).quantity;
     	((Software)(root.data)).price = ((Software)(target)).price;
     }
-
     return root;
   }
 
+  // inorder traversal of BST
   void inorder() {
     inorderRec(root);
   }
 
-  // Inorder Traversal
   void inorderRec(BSTNode<T> root) {
     if (root != null) {
       inorderRec(root.left);
@@ -45,16 +52,17 @@ class BinarySearchTree<T>{
     }
   }
 
+  // delete a node from BST
   void deleteKey(T target) {
     root = deleteRec(root, target);
   }
 
   BSTNode<T> deleteRec(BSTNode<T> root, T target) {
-    // Return if the tree is empty
+    // if tree is empty
     if (root == null)
       return root;
 
-    // Find the node to be deleted
+    // delete node by its key
     String targetNode = ((Software)(target)).key;
     String rootNode = ((Software)(root.data)).key;
     
@@ -63,7 +71,7 @@ class BinarySearchTree<T>{
     else if (targetNode.compareTo(rootNode)>0)
       root.right = deleteRec(root.right, target);
     else {
-      // If the node is with only one child or no child
+     // the current node is the target node to be deleted
       if (root.left == null)
         return root.right;
       else if (root.right == null)
@@ -74,7 +82,6 @@ class BinarySearchTree<T>{
           root.key = nodeSucc.key;
       }
     }
-
     return root;
   }
 
@@ -86,24 +93,24 @@ class BinarySearchTree<T>{
 	  return node;
   }
   
+  // search node given a Software object
   public BSTNode<T> search(T target) {
 	 return searchRec(root, target);
   }
   
   // Overloading method
+  // search node given Software name
   public BSTNode<T> search(String name) {
 		 return searchRec(root, name);
   }
   
   public BSTNode<T> searchRec(BSTNode<T> root, T target)
   {
-      // Base Cases: root is null or key is present at root
       if (root==null || root.key.equals(((Software)(target)).key))
           return root;
    
       String targetNode = ((Software)(target)).key;
       String rootNode = ((Software)(root.data)).key;
-      // Key is greater than root's key
       if (targetNode.compareTo(rootNode)<0)
          return searchRec(root.left, target);
       else if (targetNode.compareTo(rootNode)>0)
@@ -112,15 +119,14 @@ class BinarySearchTree<T>{
     	  return root;
   }
   
-  // overloading
+  // overloading method
+  // search node given Software name
   public BSTNode<T> searchRec(BSTNode<T> root, String name)
   {
-      // Base Cases: root is null or key is present at root
       if (root==null)
           return null;
    
       String rootNode = ((Software)(root.data)).name;
-      // Key is greater than root's key
       if (name.compareTo(rootNode)<0)
          return searchRec(root.left, name);
       else if (name.compareTo(rootNode)>0)
@@ -132,7 +138,7 @@ class BinarySearchTree<T>{
   public void printInventory() {
 	    printInventory(root);
   }
-
+  // print all node in BST
   private void printInventory(BSTNode<T> node) {
 	    if (node == null) {
 	      return;
@@ -142,10 +148,13 @@ class BinarySearchTree<T>{
 	    System.out.println();
 	    printInventory(node.right);
 }
-  
+ 
+  // remove any Software with quantity = 0
+  // read from Software.txt and write updated Software info into Software_tmp.txt
+  // Rename Software_tmp.txt to Software.txt and delete the old Software.txt
   public void updateFile() throws java.io.IOException {
-	  java.io.BufferedReader reader = new java.io.BufferedReader(new java.io.FileReader("C:\\Users\\USER\\eclipse-workspace\\Algo_DS\\Java_BST\\Software.txt"));
-	  java.io.BufferedWriter writer = new java.io.BufferedWriter(new java.io.FileWriter("C:\\Users\\USER\\eclipse-workspace\\Algo_DS\\Java_BST\\Software_tmp.txt"));
+	  java.io.BufferedReader reader = new java.io.BufferedReader(new java.io.FileReader(FILEPATH));
+	  java.io.BufferedWriter writer = new java.io.BufferedWriter(new java.io.FileWriter(FILEPATH_TEMP));
 
 	    String line;
 	    int pos = 0;
@@ -156,7 +165,7 @@ class BinarySearchTree<T>{
 	        writer.write(line+"\n");
 	      }
 	      else {
-	        // skip entries with 0 quantity
+	        // skip Software with quantity = 0
 	        continue;
 	      }
 	      pos += line.length() + 1;
@@ -166,8 +175,8 @@ class BinarySearchTree<T>{
 
 	    // replace software with cleaned up file
 	    // we end up with Software.txt only
-	    java.io.File oldFile = new java.io.File("C:\\Users\\USER\\eclipse-workspace\\Algo_DS\\Java_BST\\Software.txt");
-	    java.io.File newFile = new java.io.File("C:\\Users\\USER\\eclipse-workspace\\Algo_DS\\Java_BST\\Software_tmp.txt");
+	    java.io.File oldFile = new java.io.File(FILEPATH);
+	    java.io.File newFile = new java.io.File(FILEPATH_TEMP);
 	    if (oldFile.delete()) {
 	      newFile.renameTo(oldFile);
 	    }
