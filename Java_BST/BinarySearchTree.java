7fba15000261faae37c0185f71334d6ce1738798
chewzzz <chewziqing@gmail.com>
2022-12-16 12:48:32 +0800

    Add main prog

--- Java_BST/BinarySearchTree.java
+++ Java_BST/BinarySearchTree.java
@@ -81,25 +81,5 @@ class BinarySearchTree<T>{
     return minv;
   }
 
-  // Driver Program to test above functions
-  public static void main(String[] args) {
-	 BinarySearchTree tree = new BinarySearchTree();
 
-    tree.insert(8);
-    tree.insert(3);
-    tree.insert(1);
-    tree.insert(6);
-    tree.insert(7);
-    tree.insert(10);
-    tree.insert(14);
-    tree.insert(4);
-
-    System.out.print("Inorder traversal: ");
-    tree.inorder();
-
-    System.out.println("\n\nAfter deleting 10");
-    tree.deleteKey(10);
-    System.out.print("Inorder traversal: ");
-    tree.inorder();
-  }
 }
