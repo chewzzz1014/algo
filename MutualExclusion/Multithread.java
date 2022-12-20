bf1342071bc9070fbae0af4bd130e024f8e66060
chewzzz <chewziqing@gmail.com>
2022-12-20 23:11:47 +0800

    Complete Multithread.java

--- MutualExclusion/Multithread.java
+++ MutualExclusion/Multithread.java
@@ -1,4 +1,13 @@
-
-public class Multithread {
-
+public class Multithread extends Thread {
+	public static void main (String[]args) {
+		for (int i=0; i<10; i++) {
+			// Local variable defined in an enclosing scope must be final
+			final String count = i+"";
+			Thread t = new Thread(() -> {
+				Thread.currentThread().setName(count);
+				System.out.println("Thread "+Thread.currentThread().getName()+" is created.");
+			});
+			t.start();
+		}
+	}
 }
