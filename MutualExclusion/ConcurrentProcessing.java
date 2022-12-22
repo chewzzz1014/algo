7c9726845a9cdc894e92888d0905292681ce88f6
chewzzz <chewziqing@gmail.com>
2022-12-22 20:23:25 +0800

    Add comment

--- MutualExclusion/ConcurrentProcessing.java
+++ MutualExclusion/ConcurrentProcessing.java
@@ -1,5 +1,7 @@
+// Chew Zi Qing
 import java.util.*;
 public class ConcurrentProcessing extends Thread {
+	// to store user inputs, factorials and thread
 	static int[] allInputs = new int[5];
 	static long[] allResults = new long[5];
 	static Thread[] allThreads = new Thread[5];
@@ -7,15 +9,18 @@ public class ConcurrentProcessing extends Thread {
 	public static void main (String[]args) throws InterruptedException {
 		Scanner sc = new Scanner(System.in);
 		
+		// read input and store thread in array
 		System.out.print("Enter 5 numbers [1-10]: ");
 		for(int i=0; i<5; i++) {
 			allInputs[i] = sc.nextInt();
 			allThreads[i] = new Thread(new CountFactorial(i, allInputs[i]));
 		}
 		
+		// start all thread
 		for(Thread t: allThreads)
 			t.start();
 		
+		// ensure that all thread are completed before proceed with output
 		for(Thread t: allThreads)
 			t.join();
 		
@@ -29,10 +34,12 @@ public class ConcurrentProcessing extends Thread {
 										.length()-1)
 										.split(", "));
 		
-		System.out.print("The factorials for "+allInputsDisplay + " are "+allResultsDisplay);
+		System.out.print("The factorials for "
+						 + allInputsDisplay + " are "+allResultsDisplay);
 		sc.close();
 	}
 	
+	// class that calculates factorial
 	static class CountFactorial implements Runnable{
 		public int idx, value;
 		long result=1;
@@ -42,6 +49,8 @@ public class ConcurrentProcessing extends Thread {
 			this.value = value;
 		}
 		
+		// will run this when thread is started
+		// store result to results array
 		public void run() {
 			for(int i=1; i<=value; i++) {
 				result *= i;
--- MutualExclusion/Multithread.java
+++ MutualExclusion/Multithread.java
@@ -1,13 +1,25 @@
+// Chew Zi Qing 212360
 public class Multithread extends Thread {
-	public static void main (String[]args) {
+	public static void main (String[]args) throws InterruptedException{
 		for (int i=0; i<10; i++) {
-			// Local variable defined in an enclosing scope must be final
+			
+			// Local variable defined in an enclosing scope must be final!
 			final String count = i+"";
+			
+			// create thread and assign name
 			Thread t = new Thread(() -> {
 				Thread.currentThread().setName(count);
-				System.out.println("Thread "+Thread.currentThread().getName()+" is created.");
+				// print output that displays thread name
+				System.out.println("Thread "
+								   + Thread.currentThread().getName()
+								   +" is created.");
 			});
+			
+			// start the thread
 			t.start();
+			
+			// make sure the current thread is completed before starts next
+			t.join();
 		}
 	}
 }
