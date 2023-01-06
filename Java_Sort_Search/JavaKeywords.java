df3a82db60337b403586b77d7678fe36df8ca9a8
chewzzz <chewziqing@gmail.com>
2023-01-06 19:53:21 +0800

    Add comments into JavaKeywords class

--- Java_Sort_Search/JavaKeywords.java
+++ Java_Sort_Search/JavaKeywords.java
@@ -1,14 +1,19 @@
+// Chew Zi Qing 212360
 import java.io.*;
 import java.util.*;
 
+//class that read all Java keywords from JavaKeyword.txt and put in arraylist
 public class JavaKeywords{
 	
+	// data field for reading keywords from file
 	private BufferedReader reader;
 	private String line;
 	private String FILEPATH = 
 				"C:\\Users\\USER\\eclipse-workspace\\Algo_DS\\Java_Sort_Search\\JavaKeyword.txt";
+	// arraylist that stores all keywords
 	private ArrayList<String> keywords = new ArrayList<>();
 	
+	// insert all Java keywords into arraylist
 	public JavaKeywords() throws IOException {
 		reader = new BufferedReader(new FileReader(FILEPATH));
 		while ((line = reader.readLine()) != null) {
@@ -17,6 +22,7 @@ public class JavaKeywords{
 		reader.close();
 	}
 	
+	// return arraylist of keywords
 	public ArrayList<String> getKeyWords(){
 		return keywords;
 	}
