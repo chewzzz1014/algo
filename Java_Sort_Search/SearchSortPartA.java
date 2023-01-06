c5d97c9bedce9892e52fae773584552cc6c27288
chewzzz <chewziqing@gmail.com>
2023-01-06 19:59:06 +0800

    Add comments into SearchSortPartA class

--- Java_Sort_Search/SearchSortPartA.java
+++ Java_Sort_Search/SearchSortPartA.java
@@ -1,26 +1,35 @@
+// Chew Zi Qing 212360
+
 //import javax.lang.model.SourceVersion.isName;
 import java.io.*;
 import java.util.*;
 
+// display a list in alphabetical order of the unique Java keywords used in the input.txt
 public class SearchSortPartA {
 	public static void main (String[]args) throws IOException{
-		
+		// data field for reading keywords from file
 		String line;
 		String FILEPATH = 
 				"C:\\Users\\USER\\eclipse-workspace\\Algo_DS\\Java_Sort_Search\\input.txt";
 		BufferedReader reader = new BufferedReader(new FileReader(FILEPATH));
 		
+		// arraylist that stores all the Java keywords
 		ArrayList<String> kw = new JavaKeywords().getKeyWords();
+		// arraylist that stores keywords appeared in the program
 		ArrayList<String> appearedKeywords = new ArrayList<>();
 		
+		// read every line from input.txt
 		while((line = reader.readLine()) != null) {
+			// split sentence into words
 			String[] allWordsInSentence = line.split(" |\\. |, ");
 			
-			// not an empty line
-			// check all words in the line
+			// if it's not an empty line
 			if (allWordsInSentence.length > 0) {
+				// check all words in the line
 				for(String s: allWordsInSentence) {
+					// if it's a Java keyword
 					if (kw.contains(s.toLowerCase())){
+						// if it's its first appearance, add to arraylist
 						if (!appearedKeywords.contains(s))
 							appearedKeywords.add(s);
 					}
@@ -29,8 +38,10 @@ public class SearchSortPartA {
 		}
 		reader.close();
 		
+		// sort keywords appeared in the program
 		Collections.sort(appearedKeywords);
 		
+		// display them
 		for (String s: appearedKeywords)
 			System.out.println(s);
 	}
