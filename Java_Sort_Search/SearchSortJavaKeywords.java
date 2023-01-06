1c873e6cc050777c1e8cac96b4641ee9af09a185
chewzzz <chewziqing@gmail.com>
2023-01-06 13:28:48 +0800

    Add JavaKeyword.txt and read from the file

--- Java_Sort_Search/SearchSortJavaKeywords.java
+++ Java_Sort_Search/SearchSortJavaKeywords.java
@@ -1,7 +1,15 @@
-import javax.lang.model.SourceVersion.isName;
+//import javax.lang.model.SourceVersion.isName;
+import java.io.*;
 
 public class SearchSortJavaKeywords {
-	public static void main (String[]args) {
+	public static void main (String[]args) throws IOException{
+		String line;
+		String FILEPATH = "C:\\Users\\USER\\eclipse-workspace\\Algo_DS\\Java_Sort_Search\\JavaKeyword.txt";
 		
+		BufferedReader reader = new BufferedReader(new FileReader(FILEPATH));
+		while ((line = reader.readLine()) != null) {
+			System.out.println(line);
+		}
+		reader.close();
 	}
 }
