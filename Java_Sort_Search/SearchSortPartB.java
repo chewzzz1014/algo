015b9308dfea006b9decaacc7c047513aa19befd
chewzzz <chewziqing@gmail.com>
2023-01-06 20:09:04 +0800

    Add comments into SearchSortPartB class

--- Java_Sort_Search/SearchSortPartB.java
+++ Java_Sort_Search/SearchSortPartB.java
@@ -1,4 +1,4 @@
-//import javax.lang.model.SourceVersion.isName;
+//Chew Zi Qing 212360
 import java.io.*;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -6,28 +6,37 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.TreeMap;
 
+//display a list in alphabetical order of the unique Java keywords used in the input.txt
+//display also number of occurence
 public class SearchSortPartB {
 	public static void main (String[]args) throws IOException{
-		
+		// data field for reading keywords from file
 		String line;
 		String FILEPATH = 
 				"C:\\Users\\USER\\eclipse-workspace\\Algo_DS\\Java_Sort_Search\\input.txt";
 		BufferedReader reader = new BufferedReader(new FileReader(FILEPATH));
 		
+		// arraylist that stores all the Java keywords
 		ArrayList<String> kw = new JavaKeywords().getKeyWords();
+		// map store keywords and their #occurence
 		Map<String, Integer> map = new HashMap<>();
 		
+		// read every line from input.txt
 		while((line = reader.readLine()) != null) {
+			// split sentence into words
 			String[] allWordsInSentence = line.split(" |\\. |, ");
 			
-			// not an empty line
-			// check all words in the line
+			// if it's not an empty line
 			if (allWordsInSentence.length > 0) {
+				//check all words in the line
 				for(String s: allWordsInSentence) {
+					// if it's a Java keyword
 					if (kw.contains(s.toLowerCase())){
 						if (map.containsKey(s))
+							// increase number of occurence
 							map.put(s, map.get(s)+1);
 						else
+							// add a new map item
 							map.put(s, 1);
 					}
 				}
@@ -35,13 +44,12 @@ public class SearchSortPartB {
 		}
 		reader.close();
 
-		// TreeMap will sort the keys in map
+		// TreeMap will sort the keys in map in ascending order
 		TreeMap<String, Integer> treeMap = new TreeMap<>(map);
 		
+		// display keywords and their #occurence
 		for (var entry : treeMap.entrySet()) {
 		    System.out.println(entry.getKey() + " " + entry.getValue());
 		}
-		
-		//String[] allKeywords = treeMap.keySet().toArray(new String[treeMap.size()]);
 	}
 }
