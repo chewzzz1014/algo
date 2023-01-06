821ba789540118fd457e502930d7fe02a3cc5916
chewzzz <chewziqing@gmail.com>
2023-01-06 14:19:30 +0800

    Complete part b

--- Java_Sort_Search/SearchSortJavaKeywords.java
+++ Java_Sort_Search/SearchSortJavaKeywords.java
@@ -9,29 +9,40 @@ import java.util.TreeMap;
 public class SearchSortJavaKeywords {
 	public static void main (String[]args) throws IOException{
 		
+		int lineNum = 1;
 		String line;
 		String FILEPATH = 
 				"C:\\Users\\USER\\eclipse-workspace\\Algo_DS\\Java_Sort_Search\\input.txt";
 		BufferedReader reader = new BufferedReader(new FileReader(FILEPATH));
 		
 		ArrayList<String> kw = new JavaKeywords().getKeyWords();
-		Map<Integer, Integer> map = new HashMap<>();
+		Map<String, Integer> map = new HashMap<>();
 		
 		while((line = reader.readLine()) != null) {
 			String[] allWordsInSentence = line.split(" |\\. |, ");
-			System.out.println(Arrays.toString(allWordsInSentence));
 			
 			// not an empty line
+			// check all words in the line
 			if (allWordsInSentence.length > 0) {
 				for(String s: allWordsInSentence) {
-					System.out.println(s + " " + kw.contains(s.toLowerCase()));
+					if (kw.contains(s.toLowerCase())){
+						if (map.containsKey(s))
+							map.put(s, map.get(s)+1);
+						else
+							map.put(s, 1);
+					}
 				}
 			}
+			lineNum ++;
 		}
 
-		TreeMap<Integer, Integer> treeMap = new TreeMap<>(map);
+		// TreeMap will sort the keys in map
+		TreeMap<String, Integer> treeMap = new TreeMap<>(map);
 		
-		// record all processes arrival time in array
-		String[] allKeywords = treeMap.keySet().toArray(new String[treeMap.size()]);
+		for (var entry : treeMap.entrySet()) {
+		    System.out.println(entry.getKey() + " " + entry.getValue());
+		}
+		
+		//String[] allKeywords = treeMap.keySet().toArray(new String[treeMap.size()]);
 	}
 }
