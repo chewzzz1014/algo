73a18e578b0f99c4062bc2a2b4594321acfe8765
chewzzz <chewziqing@gmail.com>
2024-02-22 15:20:01 +0800

    travesal and bst

--- jovian-course/binary_search_tree.py
+++ jovian-course/binary_search_tree.py
@@ -54,3 +54,39 @@ def display_keys(node, space='\t', level=0):
     display_keys(node.right, space, level + 1)
     print(space * level + str(node.key))
     display_keys(node.left, space, level + 1)
+
+
+# travesal: in order, pre order, post order
+def traverse_in_order(node):
+    if node is None:
+        return []
+    return(traverse_in_order(node.left) +
+           [node.key] +
+           traverse_in_order(node.right))
+def traverse_pre_order(node):
+    if node is None:
+        return []
+    return ([node.key] +
+            traverse_pre_order(node.left) +
+            traverse_pre_order(node.right))
+def traverse_post_order(node):
+    if node is None:
+        return []
+    return (traverse_post_order(node.left) +
+           traverse_post_order(node.right) +
+           [node.key])
+
+
+# check is bst, return min and max node
+def is_bst(node):
+    def remove_none(arr):
+        return [x for x in arr if x is not None]
+    if node is None:
+        return True, None, None
+    is_bst_l, min_l, max_l = is_bst(node.left)
+    is_bst_r, min_r, max_r = is_bst(node.right)
+    is_bst_node = (is_bst_l and is_bst_r) and (max_l is None or node.key>max_l) and (min_r is None or node.key<min_r)
+    min_key = min(remove_none([min_l, node.key, min_r]))
+    max_key = max(remove_none([max_l, node.key, max_r]))
+    return is_bst_node, min_key, max_key
+
